8cb78844963ac7b8155d359c1212fd9b52527726
Collins Tatang <makungongcollins@gmail.com>
2021-08-25 10:41:20 +0100

    Add Books component action

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,7 +1,58 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as UUID } from 'uuid';
+import { addBook, allBooks, removeBook } from '../../redux/books/books';
+
 export default function Books() {
+  const dispatch = useDispatch();
+  const books = useSelector(allBooks);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const setBookTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const setBookAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const submitToBookStore = () => {
+    const newBook = {
+      id: UUID(),
+      title,
+      author,
+    };
+    dispatch(addBook(newBook));
+  };
+
+  const deleteBookFromStore = (e) => {
+    dispatch(removeBook({ id: e.target.id }));
+  };
+
   return (
     <div>
       <h3>All Books</h3>
+      <div>
+        <ul>
+          { books.map((book) => (
+            <li key={book.id}>
+              <span className="">{book.title}</span>
+              <button id={book.id} type="button" onClick={deleteBookFromStore}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <form>
+        <h4>Add New Book</h4>
+        <input placeholder="Title" onChange={setBookTitle} />
+        <br />
+        <br />
+        <input placeholder="Author" onChange={setBookAuthor} />
+        <br />
+        <br />
+        <button type="button" onClick={submitToBookStore}>Add Book</button>
+      </form>
     </div>
   );
 }
