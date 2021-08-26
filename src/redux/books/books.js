303cff199bb91ae5e08934a6d997a1183d6a158b
Collins Tatang <makungongcollins@gmail.com>
2021-08-26 03:33:18 +0100

    Refactor add/remove book persist update to the server

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,3 +1,7 @@
+import {
+  getBooks, createBook, deleteBook,
+} from '../../api';
+
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
@@ -22,6 +26,31 @@ const reducer = (state = initialState, action) => {
     default:
       return state;
   }
+};
+
+export const fetchBooks = async (dispatch) => {
+  let books = await getBooks();
+  if (books.trim().length > 0) {
+    books = JSON.parse(books);
+    Object.keys(books).forEach((book) => {
+      dispatch(addBook({
+        id: book,
+        title: books[book][0].title.split('&|&|&')[0],
+        author: books[book][0].title.split('&|&|&')[1],
+        category: books[book][0].category,
+      }));
+    });
+  }
+};
+
+export const addBookToStore = (book) => async function addBookThunk(dispatch) {
+  await createBook(book);
+  dispatch(addBook(book));
+};
+
+export const removeBookFromStore = (book) => async function removeBookThunkj(dispatch) {
+  await deleteBook(book);
+  dispatch(removeBook(book));
 };
 
 export const allBooks = (state) => state.booksReducer;
